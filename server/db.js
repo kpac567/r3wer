61812de30e1933be3c8956392e4e438d67e154c5
kpac567 <71659832+kpac567@users.noreply.github.com>
2020-12-15 11:41:37 -0500

    p

--- server/db.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require("mongoose");
-const connection = mongoose.connect("mongodb://localhost:27017/cards", { useNewUrlParser: true });
-module.exports = connection;
--- server/models/Card.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose')
-
-const cardSchema = mongoose.Schema({
-    name: String,
-    borough: String,
-    imgUrl: String,
-})
-
-module.exports = mongoose.model('Card', cardSchema);
