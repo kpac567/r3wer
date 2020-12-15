4f8735b53cbf4c9a8c323dd645fcae1aab531434
kpac567 <71659832+kpac567@users.noreply.github.com>
2020-12-15 11:48:03 -0500

    po

--- server.js
+++ server.js
@@ -1,75 +1,87 @@
-const path = require('path');
-const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers
-} = require('./utils/users');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const cors = require('cors');
+const hbs = require('express-handlebars');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+// get config of database
+mongoose.connect(config.database);
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'Crush Bot';
-
-// Run when client connects
-io.on('connection', socket => {
-  socket.on('joinRoom', ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
-
-    socket.join(user.room);
-
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to Crush 19!'));
-
-    // Broadcast when a user connects
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        'message',
-        formatMessage(botName, `${user.username} has joined the chat`)
-      );
-
-    // Send users and room info
-    io.to(user.room).emit('roomUsers', {
-      room: user.room,
-      users: getRoomUsers(user.room)
-    });
-  });
-
-  // Listen for chatMessage
-  socket.on('chatMessage', msg => {
-    const user = getCurrentUser(socket.id);
-
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
-  });
-
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    const user = userLeave(socket.id);
-
-    if (user) {
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      // Send users and room info
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      });
-    }
-  });
+// on connect database
+mongoose.connection.on('connected', () => {
+    console.log('mongoose connected as:' + config.database);
 });
 
-const PORT = process.env.PORT || 3000;
+// on reject database
+mongoose.connection.on('error', (err) => {
+    console.log('database Rejected ...' + err);
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// initialization app variable with express
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// chats api routes
+const chats = require('./routes/api/chats');
+
+// Port Number
+const port = 5000;
+
+// CORS Middleware
+app.use(cors());
+
+// set static folder
+// To serve static files such as images, CSS files, and JavaScript files
+// app.use(express.static(path.join(__dirname, 'public')));// absolute path
+/**
+ * set view engine for backend test, that is not necessary after added react front end
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir:__dirname+'/views/layouts'}));
+app.set('view engine', 'hbs');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+// using chats routes as localhost:portnumber/chats/nextpath.
+app.use('/chats', chats);
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+    console.log('A user connected..........');
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+        console.log('A user disconnected........');
+    });
+});
+
+users = [];
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    socket.on('setMsgBy', function(data) {
+        console.log(data);
+        // check this msgBy in chatroom of database
+            users.push(data);
+            socket.emit('userSet', {msgBy: data});
+    });
+
+    socket.on('msg', function(data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
+    });
+});
+
+// index route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// start server here
+http.listen(port, () => {
+    console.log('SERVER started on port number: '+port);
+});
+
